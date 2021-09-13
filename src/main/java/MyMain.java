7f29ecd4c79dcffeaefde7870bcda71fa000221b
natewong <nwwong17@gmail.com>
2021-09-13 12:10:27 -0700

    Created modulo functions for APCS classwork

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,7 @@ public class MyMain {
     // Ex. divisibleByFour(16) -> true
     //     divisibleByFour(5) -> false
     public static boolean divisibleByFour(int num) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        return num%4==0;
     }
 
     // This method prints every number from 1 up to and including the input biggest
@@ -14,15 +13,27 @@ public class MyMain {
     // 6 7 8 9 10
     // ...
     public static void printNumsUpTo(int biggest) {
-        // REPLACE THIS WITH YOUR CODE
+        for(int i=1; i<biggest+1; i++)
+        {
+            System.out.print(i + " ");
+            if(i%5==0)
+            {
+                System.out.println();
+            }
+        }
     }
 
     // This method is given an input x, and it adds up the sum of its digits
     // Ex. sumDigits(61) -> 7 because 6 + 1 = 7
     //     sumDigits(123) -> 6 because 1 + 2 + 3 = 6
     public static int sumDigits(int x) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int sum=0;
+        while(x>0)
+        {
+            sum+=x%10;
+            x/=10;
+        }
+        return sum;
     }
 
     // Bonus: This method returns true if the input num is a "self-divisor",
@@ -35,8 +46,15 @@ public class MyMain {
     // Note that 0 is not considered to be a divisor of any number, so any number containing a 0
     // digit is NOT a self-divisor.
     public static boolean isSelfDivisor(int num) {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        int realnum=num;
+        while(num>0)
+        {
+            if(num%10==0||realnum%(num%10)!=0) {
+                return false;
+            }
+            num/=10;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
